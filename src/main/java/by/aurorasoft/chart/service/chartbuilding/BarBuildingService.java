8050f969d008c1124bcdd1e77481654e0e50a253
Vlad Zuev <vladzuev.00@mail.ru>
2023-05-12 14:09:44 +0300

    refactor BarBuildingService

--- src/main/java/by/aurorasoft/chart/service/chartbuilding/BarBuildingService.java
+++ src/main/java/by/aurorasoft/chart/service/chartbuilding/BarBuildingService.java
@@ -4,19 +4,25 @@ import by.aurorasoft.chart.model.chart.Bar;
 import by.aurorasoft.chart.model.series.BarSeries;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public final class BarBuildingService extends AbstractBarBuildingService<Bar> {
+    private static final String DEFAULT_SERIES_NAME = "";
 
     public BarBuildingService() {
         super(Bar.class);
     }
 
     @Override
-    protected org.icepear.echarts.charts.bar.BarSeries mapToEchartsBarSeries(BarSeries barSeries) {
+    protected org.icepear.echarts.charts.bar.BarSeries mapToBuilderSeries(BarSeries series) {
         return new org.icepear.echarts.charts.bar.BarSeries()
-                //TODO: refactor
-                .setName(barSeries.findName().get())
-                .setData(barSeries.getValue())
-                .setAnimation(false);
+                .setName(findName(series))
+                .setData(series.getValue());
+    }
+
+    private static String findName(BarSeries series) {
+        final Optional<String> optionalName = series.findName();
+        return optionalName.orElse(DEFAULT_SERIES_NAME);
     }
 }
