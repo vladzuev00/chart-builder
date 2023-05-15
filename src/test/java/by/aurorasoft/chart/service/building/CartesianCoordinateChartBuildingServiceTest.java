3911e6a56292c2726904182e02b0d1c989d64886
Vlad Zuev <vladzuev.00@mail.ru>
2023-05-15 15:39:15 +0300

    adding empty tests

--- src/test/java/by/aurorasoft/chart/service/building/CartesianCoordinateChartBuildingServiceTest.java
+++ src/test/java/by/aurorasoft/chart/service/building/CartesianCoordinateChartBuildingServiceTest.java
@@ -1,7 +1,12 @@
 package by.aurorasoft.chart.service.building;
 
+import org.junit.Test;
+
 public final class CartesianCoordinateChartBuildingServiceTest {
 
-
+    @Test
+    public void a() {
+        throw new RuntimeException();
+    }
 
 }
