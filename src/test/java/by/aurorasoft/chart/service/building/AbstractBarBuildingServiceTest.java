599b82bd0860df2d8b688549a02bf0ef214e8579
Vlad Zuev <vladzuev.00@mail.ru>
2023-05-15 12:42:07 +0300

    refactor AbstractBarBuildingServiceTest

--- src/test/java/by/aurorasoft/chart/service/building/AbstractBarBuildingServiceTest.java
+++ src/test/java/by/aurorasoft/chart/service/building/AbstractBarBuildingServiceTest.java
@@ -1,14 +1,21 @@
 package by.aurorasoft.chart.service.building;
 
 import by.aurorasoft.chart.model.chart.AbstractBar;
+import lombok.Getter;
 import org.icepear.echarts.Bar;
 import org.icepear.echarts.charts.bar.BarSeries;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public final class AbstractBarBuildingServiceTest {
-    private final AbstractBarBuildingService<?> buildingService = new TestBarBuildingService();
+    private TestBarBuildingService buildingService;
+
+    @Before
+    public void initializeBuildingService() {
+        this.buildingService = new TestBarBuildingService();
+    }
 
     @Test
     public void builderShouldBeCreated() {
@@ -16,7 +23,24 @@ public final class AbstractBarBuildingServiceTest {
         assertNotNull(actual);
     }
 
+    @Test
+    public void sourceSeriesValueShouldBeMappedToBuilderSeriesValue() {
+        final Number[] givenSourceSeriesValues = new Number[]{1, 2, 3};
+
+        final Number[] actual = this.buildingService.mapToBuilderSeriesValue(givenSourceSeriesValues);
+        assertSame(givenSourceSeriesValues, actual);
+    }
+
+    @Test
+    public void builderSeriesShouldBeCreated() {
+        final BarSeries actual = this.buildingService.createBuilderSeries();
+        assertNotNull(actual);
+        assertTrue(this.buildingService.isBuilderSeriesConfigured());
+    }
+
+    @Getter
     private static final class TestBarBuildingService extends AbstractBarBuildingService<AbstractBar> {
+        private boolean builderSeriesConfigured;
 
         public TestBarBuildingService() {
             super(AbstractBar.class);
@@ -24,7 +48,7 @@ public final class AbstractBarBuildingServiceTest {
 
         @Override
         protected void configureBuilderSeries(BarSeries builderSeries) {
-            throw new UnsupportedOperationException();
+            this.builderSeriesConfigured = true;
         }
 
         @Override
